fce4397e60a7a6f363fbbd390d082a96f6c6cfb0
James Woodall <jamesikanos@gmail.com>
2025-01-06 15:18:56 +0000

    Updated header script

--- .devops/add-header.js
+++ .devops/add-header.js
@@ -10,24 +10,23 @@ if (!globPattern) {
 const HEADER = `/**
  * Copyright (C) 2024 RealWear, Inc.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */`;
 
 function addHeaderToFile(filePath) {
   const content = fs.readFileSync(filePath, 'utf-8');
   if (!content.startsWith(HEADER)) {
-    fs.writeFileSync(filePath, HEADER + "\n" + content);
+    fs.writeFileSync(filePath, HEADER + '\n' + content);
     console.log(`Header added to: ${filePath}`);
   }
 }
@@ -41,11 +40,10 @@ const files = glob.sync(globPattern, (err, files) => {
   }
 });
 
-files.forEach(file => {
+files.forEach((file) => {
   try {
     addHeaderToFile(file);
-  }
-  catch (e) {
+  } catch (e) {
     console.error(e);
   }
-})
+});
